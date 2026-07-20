4d396bd4959744ab8f2d76eaab958f7f86ed31d0
wololo4 <59884953+wololo4@users.noreply.github.com>
2026-07-20 12:34:54 -0400

    Create calendar.py

---
+++ utils/calendar.py
@@ -0,0 +1,9 @@
+def create_calendar():
+    cal = Calendar()
+    cal.add("prodid", "-//Hockey Calendar//")
+    cal.add("version", "2.0")
+    return cal
+
+
+def event_id(event):
+    return str(event.get("UID")) + str(event.get("DTSTART"))
