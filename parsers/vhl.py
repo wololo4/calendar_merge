ac3653f90789945c7c80f688ff0fb605a32235cb
wololo4 <59884953+wololo4@users.noreply.github.com>
2026-07-21 12:29:40 -0400

    Update vhl.py

--- parsers/vhl.py
+++ parsers/vhl.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
 from icalendar import Calendar, Event
-from datetime import 
+from datetime import datetime
 
 def parse_vhl_html(html, team_name):
     soup = BeautifulSoup(html, "html.parser")
