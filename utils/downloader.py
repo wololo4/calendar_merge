6dec8ab53b24e67173eb650632399807bb943777
wololo4 <59884953+wololo4@users.noreply.github.com>
2026-08-05 11:19:21 -0400

    Test

--- utils/downloader.py
+++ utils/downloader.py
@@ -3,7 +3,6 @@ import requests
 import json
 import re
 import cloudscraper
-import traceback
 from bs4 import BeautifulSoup
 from icalendar import Calendar
 
@@ -87,15 +86,20 @@ def download_single_feed(feed_info):
         # Publication Sports
         # ============================
         if parser == "publicationsports":
-            try:
-                scraper = cloudscraper.create_scraper()
-                html = scraper.get(url).text
-                calendar = parse_publicationsports(html, team_filter, league)
-                return league, team_name, calendar
-            except Exception:
-                traceback.print_exc()
+            #try:
+            scraper = cloudscraper.create_scraper()
+            response = scraper.get(url)
+
+            print("DEBUG URL:", url)
+            print("DEBUG STATUS:", response.status_code)
+            print("DEBUG HEADERS:", response.headers)
+            print("DEBUG HTML FIRST 2000:", response.text[:2000])
+            html = scraper.get(url).text
+            calendar = parse_publicationsports(html, team_filter, league)
+            return league, team_name, calendar
+            #except Exception:
                 #print("Error parsing Publication Sports JSON:", e)
-                return league, team_name, None
+                #return league, team_name, None
 
         # ============================
         # NCAA / SIDEARM JSON
