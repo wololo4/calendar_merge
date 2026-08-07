fd946b75fa83903443ef437925879f629065dcda
wololo4 <59884953+wololo4@users.noreply.github.com>
2026-08-06 23:02:13 -0400

    Update

--- parsers/liiga.py
+++ parsers/liiga.py
@@ -27,7 +27,7 @@ def parse_liiga_json_to_calendar(games):
         game_center = f"https://liiga.fi/en/game/{season}/{uid}"
 
         event = Event()
-        event.add("SUMMARY", f"🏒 | {home} vs {away}")
+        event.add("SUMMARY", f"🏒 | {away} @ {home}")
         event.add("DTSTART", dt)
         dt_end = dt + timedelta(hours=2, minutes=30)
         event.add("DTEND", dt_end)
