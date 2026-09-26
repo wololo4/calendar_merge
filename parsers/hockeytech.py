51eb0b93e7693549fb4a6e078dcd1d6bece50ba6
wololo4 <59884953+wololo4@users.noreply.github.com>
2026-09-25 22:11:03 -0400

    Change streaming service from Victory+ to YouTube

--- parsers/hockeytech.py
+++ parsers/hockeytech.py
@@ -191,7 +191,7 @@ def parse_hockeytech(json_data, team_filter, team_name):
             description_items.append(f"TV: {', '.join(broadcasters)}")
 
         if league == "whl":
-            streaming = "Victory+"
+            streaming = "YouTube"
         else:
             streaming = flo_link if flo_link else None
         description_items.append(f"Streaming: {streaming}" if streaming else "Streaming link not yet available")
